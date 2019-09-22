97177235ec6f33274c06daeb689df63f409384ae
harprit <harprits10@gmail.com>
2019-09-22 20:07:21 +0800

    added apis for delete and update

--- pizza-api/api.js
+++ pizza-api/api.js
@@ -5,6 +5,8 @@ const api = new Api()
 
 const getPizzas = require('./handlers/get-pizzas')
 const createOrder = require('./handlers/create-order')
+const updateOrder = require('./handlers/update-order')
+const deleteOrder = require('./handlers/delete-order')
 
 api.get('/', () => 'Welcome to Pizza API')
 
@@ -20,10 +22,21 @@ api.get('/pizzas/{id}', (request) => {
 
 api.post('/orders', (request) => {
     return createOrder(request.body)
-},
-    {
-        success: 201,
-        error: 400
-    })
+}, {
+    success: 201,
+    error: 400
+})
+
+api.put('/orders/{id}', (request) => {
+    return updateOrder(request.pathParams.id, request.body)
+}, {
+    error: 400
+})
+
+api.delete('/orders/{id}', (request) => {
+    return deleteOrder(request.pathParams.id)
+}, {
+    error: 400
+})
 
 module.exports = api
